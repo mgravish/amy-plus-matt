d0b384ef57f627afbdf8100ae4b44f8de6e8ab2e
Matt <mgravish@gmail.com>
2018-02-20 00:54:13 -0800

    touch fix

--- js/mattamy.js
+++ js/mattamy.js
@@ -71,7 +71,7 @@ $(document).ready(function() { // better to use $(document).ready(function(){
     var navdown = false;
     $('.mobile-menu').on('touchend click', function(e) {
             if(e.type == 'touchend'){
-                //$(this).off('click');
+                $(this).off('click');
             }
             if(!navdown){
                 navdown=true;
