0aebfb23172df6179cb93b9e2ac76f0024d6f39c
Matt <mgravish@gmail.com>
2018-04-10 00:30:55 -0700

    Lots of updates

--- js/scroll-slideshow.js
+++ js/scroll-slideshow.js
@@ -6,38 +6,25 @@ $(document).scroll(function () {
     
     var h = $( document ).height()-400;
     var y = $(this).scrollTop();
-
-    if($( window ).width()<960) {
-        if(y>60 && !shrunk) {
-            console.log("shrinking");
-            shrunk = true;
-            $('.left-box').css('height','180');
-            $('.left-box').css('box-shadow','0px 0px 73px 0px rgba(0, 0, 0, 0.17)');
-            $('.left-box').css('-webkit-box-shadow','0px 0px 73px 0px rgba(0, 0, 0, 0.17)');
-            $('.left-box').css('-moz-box-shadow','0px 0px 73px 0px rgba(0, 0, 0, 0.17)');
-        }
-
-        if(y<60 && shrunk){
-            console.log("at zero");
-            shrunk = false;
-            $('.left-box').css('height','340');
-            $('.left-box').css('box-shadow','0px 0px 73px 0px rgba(0, 0, 0, 0.0)');
-            $('.left-box').css('-webkit-box-shadow','0px 0px 73px 0px rgba(0, 0, 0, 0.0)');
-            $('.left-box').css('-moz-box-shadow','0px 0px 73px 0px rgba(0, 0, 0, 0.0)');
-        }
-    }
+    var s = $('#story').offset().top;
+    var v = $('#venue').offset().top;
+    var h2 = $('#hotel').offset().top;
     
-    if (y > h/4) {
+    if (y > s/2) {
+        $('.title-block').fadeIn({});
         $('#img2').fadeIn({});
     }
-    else {$('#img2').fadeOut('fast')};
+    else {
+        $('#img2').fadeOut('fast')
+        $('.title-block').fadeOut('fast');
+    };
     
-    if (y > 2*(h/4)) {
+    if (y > v) {
         $('#img3').fadeIn({});
     }
     else {$('#img3').fadeOut('fast')};
     
-    if (y > 3*(h/4)) {
+    if (y > h2) {
         $('#img4').fadeIn({});
     }
     else {$('#img4').fadeOut('fast')};
